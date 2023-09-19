8e59802a355696d00bc28715b4ba295ed5be5619
shakil88818 <shakil88818@gmail.com>
2023-09-20 01:19:39 +0600

    DealOfTheWeek Section update

---
+++ src/components/Home/DealOfTheWeek/DealOfTheWeek.jsx
@@ -0,0 +1,34 @@
+import {} from 'react'
+import { Row, Col } from 'react-bootstrap'
+
+import dealOfTheWeek_1 from '../../../assets/Image/dealOfTheWeek_1.png'
+
+
+const DealOfTheWeek = () => {
+  return (
+    <>
+      <div className="container-xxl sectionGap_top dealBg">
+        <Row>
+            <Col lg={6} md={6} sm={12}>
+                <div>
+                <img className="dealImg" src={dealOfTheWeek_1} />
+                </div>
+            </Col>
+
+
+            <Col lg={6} md={6} sm={12}>
+                <div className='dealPadding'>
+                    <p className='exSmallHeading'>DEAL OF THE WEEK</p>
+                    <h2 className='dealHeding'>Sk Mart White T-shirt</h2>
+                    <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+                    <button className='button_B mt-5'>Shop Now</button>
+                    <p className='dealSmallDesc mt-5'>Limited time offer. The deal will expires <span className='dealSmallBg'>on december, 2023</span> HURRY UP!</p>
+                </div>
+            </Col>
+        </Row>
+      </div>
+    </>
+  )
+}
+
+export default DealOfTheWeek
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import HomeUp from '../components/Home/HomeUp/HomeUp.jsx'
 import NewSeason from '../components/Home/NewSeason/NewSeason.jsx'
 import SummerCollection from '../components/Home/SummerCollection/SummerCollection.jsx'
+import DealOfTheWeek from '../components/Home/DealOfTheWeek/DealOfTheWeek.jsx'
 import NewSeason2 from '../components/Home/NewSeason2/NewSeason2.jsx'
 import CatagoriesProduct from '../components/Home/CatagoriesProduct/CatagoriesProduct.jsx'
 import ClientReview from '../components/Home/ClientReview/ClientReview.jsx'
@@ -18,6 +19,7 @@ const Home = () => {
       <HomeUp />
       <NewSeason />
       <SummerCollection />
+      <DealOfTheWeek />
       <NewSeason2 />
       <CatagoriesProduct />
       <ClientReview />
